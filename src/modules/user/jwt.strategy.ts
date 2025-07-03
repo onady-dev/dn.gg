cfd428292b196ff2907842d869ca7552a75d80f1
onady <your.email@example.com>
2025-07-03 17:31:24 +0900

    fix: dockerfile 수정

--- src/modules/user/jwt.strategy.ts
+++ src/modules/user/jwt.strategy.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { config } from 'dotenv';
-
-config();
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
